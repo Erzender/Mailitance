825d80ad4d4be7e1482708bfc06fa575373dc941
Erzender <lukas.parant@epitech.eu>
2021-09-02 23:04:49 +0200

    login + varisous fixes

--- app/src/duck/reducer.js
+++ app/src/duck/reducer.js
@@ -1,12 +1,32 @@
 import { NAV_COLLECT } from "../const";
 
 const initialState = {
+  token: null,
+  loginError: null,
+  loginFetching: false,
   navbar: { mobileShow: false, modal: { open: false } },
-  nav: { [NAV_COLLECT]: {} },
+  nav: { [NAV_COLLECT]: {} }
 };
+
+let token = localStorage.getItem("token");
+if (!!token) {
+  initialState.token = token;
+}
 
 const root = (state = initialState, action) => {
   switch (action.type) {
+    case "LOGIN_REQUEST":
+      return { ...state, loginFetching: true };
+    case "LOGIN_RESOLVE":
+      if (action.res.success) {
+        localStorage.setItem("token", action.res.token);
+      }
+      return {
+        ...state,
+        token: action.res.success ? action.res.token : null,
+        loginError: !action.res.success ? true : null,
+        loginFetching: false
+      };
     case "NAVBAR_TOGGLE_MODAL":
       return {
         ...state,
@@ -17,19 +37,19 @@ const root = (state = initialState, action) => {
             open:
               action.value !== undefined
                 ? action.value
-                : !state.navbar.modal.open,
-          },
-        },
+                : !state.navbar.modal.open
+          }
+        }
       };
     case "NAVBAR_MOBILE_TOGGLE":
       return {
         ...state,
-        navbar: { ...state.navbar, mobileShow: !state.navbar.mobileShow },
+        navbar: { ...state.navbar, mobileShow: !state.navbar.mobileShow }
       };
     case "NAVBAR_NAV_TO":
       return {
         ...state,
-        nav: action.route,
+        nav: action.route
       };
     default:
       return state;
