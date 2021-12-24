132fdbd0ebaa66194b9d5c6b5b808d10509c6d69
Jérémie Dubuis <jeremie.dubuis@gmail.com>
2021-12-24 03:51:00 +0100

    :sparkles: Add CSV download button

--- app/views/group/Group/Group.jsx
+++ app/views/group/Group/Group.jsx
@@ -4,11 +4,13 @@ import {groupByIdSelector, selectedGroupSelector} from "../../../redux/groups/gr
 import {contactsFetch} from "../../../redux/contacts/contactsActions";
 import {ContactsList} from "../../../components/lists/ContactsList/ContactsList";
 import {useRouter} from "next/router";
+import {contactsListSelector} from "../../../redux/contacts/contactsSelector";
 
 export const Group = ({groupId}) => {
   const group = useSelector(groupByIdSelector(groupId));
   const dispatch = useDispatch();
   const selectedGroup = useSelector(selectedGroupSelector)
+  const contacts = useSelector(contactsListSelector);
   const { push} = useRouter();
   useEffect(() => {
     if (groupId) dispatch(contactsFetch(groupId))
@@ -21,6 +23,19 @@ export const Group = ({groupId}) => {
 
   return group ? <main>
     <h1>Group {group.title}</h1>
+    <button onClick={() => {
+
+      const csvContent = `data:text/csv;charset=utf-8,${['Nom de famille', 'Prénom', 'E-mail', 'Téléphone', 'Âge', 'Inscrit sur les lists'].join(',')}
+      ${contacts.map(c => [c.lastname, c.firstname, c.email, c.phone, c.age, c.voteRegistration ? 1 : 0].join(',')).join("\n")}`
+
+      const link = document.createElement("a");
+      link.setAttribute("href", encodeURI(csvContent));
+      link.setAttribute("download", group.title+'.csv');
+      document.body.appendChild(link); // Required for FF
+      link.click();
+      document.body.removeChild(link);
+    }
+    }>Télécharger CSV</button>
     <ContactsList />
   </main> : 'Loading';
 }
