ca48077a168a13acadef94f063f1a43327205a9b
Jérémie Dubuis <jeremie.dubuis@gmail.com>
2021-12-24 04:40:18 +0100

    :lipstick: styles, :sparkles: search

--- app/components/lists/ContactsList/ContactsList.jsx
+++ app/components/lists/ContactsList/ContactsList.jsx
@@ -1,9 +1,12 @@
+import styles from './ContactList.module.css'
 import React, {useState} from 'react';
 import {useSelector} from "react-redux";
 import {contactsListSelector} from "../../../redux/contacts/contactsSelector";
 import {FiCheck, FiX} from "react-icons/fi";
 import {Table} from "../Table/Table";
-import styles from "../Table/Table.module.css";
+import tableStyles from "../Table/Table.module.css";
+import {StyledFormField} from "../../forms/StyledFormField/StyledFormField";
+import {Button} from "../../buttons/Button/Button";
 
 const sorters = [
   { label: 'Nom de famille', name: 'lastname'},
@@ -12,36 +15,52 @@ const sorters = [
   { label: 'Âge', name: 'age' },
 ]
 
-export const ContactsList = () => {
+export const ContactsList = ({ buttons }) => {
   const contacts = useSelector(contactsListSelector);
   const [sorter, setSorter] = useState({ name: 'lastname', order: 1});
+  const [searchParam, setSearchParam] = useState('lastname');
+  const [search, setSearch] = useState('');
 
   const sort = _sorter => {
     if (_sorter !== sorter.name) setSorter({ name: _sorter, order: 1 });
     else setSorter({ name: _sorter, order: sorter.order === 1 ? -1 : 1})
   }
-  return <Table>
-    <thead>
+  return <div className={styles.list}>
+    <header>
+      <div className={styles.search}>
+        <StyledFormField label="Filter" onChange={e => setSearchParam(e.target.value)} type="select" options={sorters.map(s => ({ label: s.label, value: s.name}))} />
+        <StyledFormField label={<span className="visually-hidden">Recherche</span> } onChange={e => setSearch(e.target.value)} type="search" />
+      </div>
+      {buttons.length && <div className={styles.buttons}>
+      {buttons.map(b => <Button {...b}/>)}
+      </div>}
+    </header>
+    <Table>
+      <thead>
       <tr>
         {sorters.map((s, i) => <th key={i}>
-         <button className={sorter.name === s.name ? sorter.order > 0 ? styles.asc: styles.dsc : null} onClick={() => sort(s.name)}>{s.label}</button>
+          <button className={sorter.name === s.name ? sorter.order > 0 ? tableStyles.asc: tableStyles.dsc : null} onClick={() => sort(s.name)}>{s.label}</button>
         </th>)}
         <th>Inscrit</th>
       </tr>
-    </thead>
-    <tbody>
-    {contacts?.sort((a,b) => {
-      console.log(sorter)
-      const order = b[sorter.name] < a[sorter.name]  ? 1 : -1;
-      return sorter.order > 0 ? order : -order;
-    }).map(c => <tr key={c.id}>
-      <td>{c.lastname}</td>
-      <td>{c.firstname}</td>
-      <td>{c.email}</td>
-      <td>{c.age}</td>
-      <td>{c.voteRegistration ? <FiCheck /> : <FiX/>}</td>
-    </tr>)}
-    </tbody>
-  </Table>
+      </thead>
+      <tbody>
+      {contacts?.filter(c => {
+        if (!search) return true;
+        return c[searchParam].toLowerCase().includes(search.toLowerCase())
+      }).sort((a,b) => {
+        console.log(sorter)
+        const order = b[sorter.name] < a[sorter.name]  ? 1 : -1;
+        return sorter.order > 0 ? order : -order;
+      }).map(c => <tr key={c.id}>
+        <td>{c.lastname}</td>
+        <td>{c.firstname}</td>
+        <td>{c.email}</td>
+        <td>{c.age}</td>
+        <td>{c.voteRegistration ? <FiCheck /> : <FiX/>}</td>
+      </tr>)}
+      </tbody>
+    </Table>
+  </div>
 
 }
--- app/views/group/Group/Group.jsx
+++ app/views/group/Group/Group.jsx
@@ -1,3 +1,4 @@
+import styles from './Group.module.css'
 import React, {useEffect, useRef, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {groupByIdSelector, selectedGroupSelector} from "../../../redux/groups/groupsSelectors";
@@ -7,8 +8,10 @@ import {useRouter} from "next/router";
 import {contactsListSelector} from "../../../redux/contacts/contactsSelector";
 import {isAdminSelector} from "../../../redux/account/accountSelectors";
 import {isOperatorSelector} from "../../../redux/sharedSelectors";
+import {Button} from "../../../components/buttons/Button/Button";
 
 export const Group = ({groupId}) => {
+  const inputFileRef = useRef();
   const group = useSelector(groupByIdSelector(groupId));
   const dispatch = useDispatch();
   const selectedGroup = useSelector(selectedGroupSelector)
@@ -28,42 +31,66 @@ export const Group = ({groupId}) => {
       push('/groupe/'+selectedGroup.id)
   }, [selectedGroup])
 
-  return group ? <main>
-    <h1>Group {group.title}</h1>
-
-    <button onClick={() => {
-
-      const csvContent = `data:text/csv;charset=utf-8,${['Nom de famille', 'Prénom', 'E-mail', 'Téléphone', 'Âge', 'Inscrit sur les lists'].join(',')}
+  const buttons = [
+    {
+      children: 'Télécharger CSV',
+      onClick: () => {
+        const csvContent = `data:text/csv;charset=utf-8,${['Nom de famille', 'Prénom', 'E-mail', 'Téléphone', 'Âge', 'Inscrit sur les lists'].join(',')}
       ${contacts.map(c => [c.lastname, c.firstname, c.email, c.phone, c.age, c.voteRegistration ? 1 : 0].join(',')).join("\n")}`
 
-      const link = document.createElement("a");
-      link.setAttribute("href", encodeURI(csvContent));
-      link.setAttribute("download", group.title+'.csv');
-      document.body.appendChild(link); // Required for FF
-      link.click();
-      document.body.removeChild(link);
+        const link = document.createElement("a");
+        link.setAttribute("href", encodeURI(csvContent));
+        link.setAttribute("download", group.title+'.csv');
+        document.body.appendChild(link); // Required for FF
+        link.click();
+        document.body.removeChild(link);
+      }
     }
-    }>Télécharger CSV</button>
+  ];
+  if (admin||isOperator) {
+    buttons.push({
+      children: 'Importer CSV',
+    })
+  }
 
-    {(admin | isOperator) && <>
-      <input type={"file"} onChange={(e) => {
+
+  return group ? <main className={styles.group}>
+    <h1>Group {group.title}</h1>
+
+    <input ref={inputFileRef} type={"file"} onChange={(e) => {
       if (e.target.files[0].name.endsWith('.csv')) {
         const reader = new FileReader();
         reader.onload = (e) => {
           const lines = e.target.result.substring(e.target.result.indexOf("\n") + 1).split(/\r?\n/g)
-          setParsedCSV( lines.map(l => {
+          const parsedCSV = lines.map(l => {
             const [lastname, firstname, email, phone, age, voteRegistration] = l.split(/\s*,\s*/g).map(r => r.replace(/^\s*|\s*$/g, ''));
             return ({lastname, firstname, email, phone, age: parseInt(age), voteRegistration : !!voteRegistration, rgpdConsent: true })
-          }))
+          })
+          contactsCreate(groupId, parsedCSV)
         };
         reader.readAsText(e.target.files[0]);
       }
-      }} />
-      {parsedCSV && <button onClick={() => {
-        dispatch(contactsCreate(groupId, parsedCSV));
-      }}>Importer CSV</button> }
+    }} />
+    <ContactsList
+      buttons={[
+        {
+          children: 'Télécharger CSV',
+          onClick: () => {
+            const csvContent = `data:text/csv;charset=utf-8,${['Nom de famille', 'Prénom', 'E-mail', 'Téléphone', 'Âge', 'Inscrit sur les lists'].join(',')}
+      ${contacts.map(c => [c.lastname, c.firstname, c.email, c.phone, c.age, c.voteRegistration ? 1 : 0].join(',')).join("\n")}`
 
-    </>}
-    <ContactsList />
+            const link = document.createElement("a");
+            link.setAttribute("href", encodeURI(csvContent));
+            link.setAttribute("download", group.title+'.csv');
+            document.body.appendChild(link); // Required for FF
+            link.click();
+            document.body.removeChild(link);
+          }
+        },
+        (admin | isOperator) && {
+          children: 'Importer CSV',
+          onCLick: () => inputFileRef.current.click()
+        }
+      ]} />
   </main> : 'Loading';
 }
